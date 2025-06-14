8279851917c40e64665a41f51727025811472a30
Zuber Zeeb <zuber@Zubers-MacBook-Pro.local>
2025-06-14 20:46:32 +0100

    created cards hover, added graphs, carousel - last section remaining before animating the graphs

--- js/main.js
+++ js/main.js
@@ -17,5 +17,44 @@ $(function() {
     $chevron.css('opacity', 0);
   });
 
+  // Accordion functionality
+  $('.accordion-header').on('click', function() {
+    var $header = $(this);
+    var expanded = $header.attr('aria-expanded') === 'true';
+    $header.attr('aria-expanded', !expanded);
+    var $content = $header.parent().find('.accordion-content');
+    if (!expanded) {
+      $content.css('max-height', $content[0].scrollHeight + 'px');
+    } else {
+      $content.css('max-height', '');
+    }
+  });
+
+
+
+  // Swiper.js initialization
+  if (document.querySelector('.swiper')) {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 30,
+      loop: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1.2,
+          centeredSlides: true,
+        },
+        1024: {
+          slidesPerView: 1.5,
+          centeredSlides: true,
+        }
+      }
+    });
+  }
+
   // Place other jQuery content here as needed
 });
